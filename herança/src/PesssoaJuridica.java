b8dfb4e556d38e4e0cc91ece9db8a522bd931a89
Gabrielly Gonçalves Mascarenhas <146665033+gabrielly-2607@users.noreply.github.com>
2024-04-30 09:46:30 -0300

    Add files via upload

---
+++ herança/src/PesssoaJuridica.java
@@ -0,0 +1,14 @@
+public class PesssoaJuridica extends Pessoa{
+
+    private String cnpj;
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getCnpj() {
+        return (cnpj);
+    }
+}
+
+ 
