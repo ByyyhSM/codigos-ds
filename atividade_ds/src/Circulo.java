69dfdfa948bf9ff1be4ba0efc6c40accbd45b3d9
Gabrielly Gonçalves Mascarenhas <146665033+ByyyhSM@users.noreply.github.com>
2024-05-22 14:16:07 -0300

    Add files via upload

---
+++ atividade_ds/src/Circulo.java
@@ -0,0 +1,19 @@
+class Circulo extends Figura {
+    private double raio;
+
+    public Circulo(double raio) {
+        this.raio = raio;
+    }
+
+    public double getRaio() {
+        return raio;
+    }
+
+    public void setRaio(double raio) {
+        this.raio = raio;
+    }
+
+    public double getDiametro() {
+        return raio * Math.PI * 2;
+    }
+}
