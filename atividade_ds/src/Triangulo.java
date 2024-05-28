17179a4b6b8fb8cc5a2d73630f11df59acb0fae2
Gabrielly Gonçalves Mascarenhas <146665033+ByyyhSM@users.noreply.github.com>
2024-05-28 16:55:02 -0300

    Update Triangulo.java

--- atividade_ds/src/Triangulo.java
+++ atividade_ds/src/Triangulo.java
@@ -1,2 +1,30 @@
-public class Triangulo {
+class Triangulo extends Figura {
+    private double base;
+
+    private double altura;
+
+    public Triangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public double getBase (){
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double area() {
+        return base * altura / 2;
+    }
 }
